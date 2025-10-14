ef319bce9f36be555a18d446b4b3d1636c6a29e5
Meleneth <meleneth@gmail.com>
2025-10-13 20:42:09 -0700

    Modify expression test to pass

--- tests/test_simple_expression.cpp
+++ tests/test_simple_expression.cpp
@@ -6,21 +6,9 @@
 #include "systems/expr_engine.hpp"
 
 TEST_CASE("ExprTk basic arithmetic works") {
-  using T = double;
-  T x = 2.0, y = 5.0;
+  auto eng = ExprEngine::compile("x * y + sqrt(x)", {{"x",2.0 },{"y",5.0}});
 
-  exprtk::symbol_table<T> sym;
-  sym.add_variable("x", x);
-  sym.add_variable("y", y);
-  sym.add_constants();
-
-  exprtk::expression<T> e;
-  e.register_symbol_table(sym);
-
-  exprtk::parser<T> p;
-  REQUIRE(p.compile("x * y + sqrt(x)", e));
-
-  REQUIRE_THAT( e.value(), Catch::Matchers::WithinRel(2.0 * 5.0 + std::sqrt(2.0), 0.001));
-  x = 9.0;
-  REQUIRE_THAT( e.value(), Catch::Matchers::WithinRel(9.0 * 5.0 + 3.0, 0.001));
+  REQUIRE_THAT( eng.eval(), Catch::Matchers::WithinRel(2.0 * 5.0 + std::sqrt(2.0), 0.001));
+  eng.set("x", 9.0);
+  REQUIRE_THAT( eng.eval(), Catch::Matchers::WithinRel(9.0 * 5.0 + 3.0, 0.001));
 }
